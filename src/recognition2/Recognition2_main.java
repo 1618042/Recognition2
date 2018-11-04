e03bd3112a00c4a33f67c3126edacf4bc8cdd8aa
1618042 <1618042@cs16042>
2018-11-04 17:43:09 +0900

    01

---
+++ src/recognition2/Recognition2_main.java
@@ -0,0 +1,27 @@
+package recognition2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+
+public class Recognition2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Recognition2_lib slib = new Recognition2_lib();
+		
+		slib.file(new File("img/fruit.jpg"));
+		
+		slib.view();
+
+
+	}
+
+}
